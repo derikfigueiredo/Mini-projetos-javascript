280586383fa4c08261eca22d7721235daf958522
Dérik Figueiredo <derikdeewk@gmail.com>
2021-06-13 21:14:49 -0300

    auto-complete-app

---
+++ auto-complete-app-learning/assets/js/script.js
@@ -0,0 +1,36 @@
+const search = document.querySelector('#search');
+const matchList = document.querySelector('#match-list');
+
+const searchStates = async searchText => {
+    const res = await fetch('../data/states.json');
+    const states = await res.json();
+
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex) || state.capital.match(regex);
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+        matchList.innerHTML = '';
+    }
+
+    outputHtml(matches); 
+};
+
+const outputHtml = matches => {
+    if (matches.length > 0) {
+        const html = matches
+            .map(
+                match => `<div class="card-body mb-1">
+                <h4>${match.name} (${match.abbr})  <span class="text-primary">${match.capital
+                }</span></h4>
+                <small>Lat: ${match.lat} / Long: ${match.long}</small>
+                </div>` 
+            ).join('')
+
+            matchList.innerHTML = html;
+    }
+}
+
+search.addEventListener('input', () => searchStates(search.value))
